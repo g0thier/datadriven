a6517f7f71b3ba7a08be10ce4c7cdaf826094545
Gauthier Rammault <gauthier.rammault@hotmail.fr>
2026-03-11 14:40:50 +0100

    Use globals in ESLint config and add globals dep
    Import the 'globals' package and configure ESLint to use Node globals (languageOptions.globals = globals.node) for functions/*.js. Add 'globals' as a devDependency and pin @eslint/js to 8.57.0 in package.json; package-lock.json updated to reflect these dependency changes. This makes ESLint recognize Node built-ins in the Cloud Functions code.

--- functions/eslint.config.js
+++ functions/eslint.config.js
@@ -1,9 +1,11 @@
 import js from "@eslint/js";
+import globals from "globals";
 
 export default [
   js.configs.recommended,
   {
-    files: ["**/*.js"],
-    rules: {}
-  }
+    languageOptions: {
+      globals: globals.node,
+    },
+  },
 ];
